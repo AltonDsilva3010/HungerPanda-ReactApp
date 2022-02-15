cd31d504b1189be956286858d99f82b18d216be2
Alton <altondsilva3010@gmail.com>
2022-02-15 23:53:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import WithNav from "./Components/WithNav";
+import WithOutNav from "./Components/WithoutNav";
+import LandingPage from "./Components/LandingPage";
+import LoginPage from "./Components/LoginPage";
+import SignUp from "./Components/SignUp";
+import GetApp from "./Components/GetApp";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route element={<WithNav />}>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<SignUp />} />
+            <Route path="/getapp" element={<GetApp />} />
+          </Route>
+          <Route element={<WithOutNav />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
